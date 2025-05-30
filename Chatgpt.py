9e277b40c024d576fa75848525d2f5fa2a610f22
Levi Legit <ahmadmashallah47@gmail.com>
2025-05-30 09:13:38 +0500

    Update Chatgpt.py

--- Chatgpt.py
+++ Chatgpt.py
@@ -4,15 +4,15 @@ import time
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 from NezukoAPI import api
-@Nezuko.on_message(filters.command(["chatgpt","ai","ask"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
-async def chat_gpt(bot, message):
+@Nezuko.on_message(filters.command(["leviai","ai","ask"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
+async def chat_gpt(bot, message): 
     
     try:
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
         if len(message.command) < 2:
             await message.reply_text(
-            "Example:**\n\n`/chatgpt Where is TajMahal?`")
-        else:
+            "Example:**\n\n`/𝚐𝚎𝚖𝚒𝚗𝚒 Thari BhenKichut?`")
+        else: 
             a = message.text.split(' ', 1)[1]
             r=api.gemini(a)["results"]
             await message.reply_text(f" {r} \n\n🎉ᴘᴏᴡᴇʀᴇᴅ ʙʏ @{BOT_USERNAME} ", parse_mode=ParseMode.MARKDOWN)     
